43dfa05bfbf7647bccfe330083ef41522ce5ea40
gillichu <gillichu@berkeley.edu>
2022-04-21 17:05:56 -0500

    consensus tree generated.

--- datafiles/run_all.sh
+++ datafiles/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[Reading in files...]"
-python select_genes.py 
+#python select_genes.py 
 
 echo "Wrote a 16S fasta file and a cytb fasta file!"
 
@@ -15,13 +15,21 @@ mafft 16s_species.fasta > 16s_species.aln
 mafft cytb_species.fasta > cytb_species.aln
 mafft all_genes.fasta > all_genes.aln
 
+mkdir -p aln_inputs
+mkdir -p mafft_alns
+
+mv *.fasta aln_inputs
+mv *.aln mafft_alns
 echo "[Running RAxML]"
 
-./../methods/osx/raxml-ng_v1.1.0_macos_x86_64/raxml-ng --msa mafft\ alns/16s_species.aln --model GTR+G
-./../methods/osx/raxml-ng_v1.1.0_macos_x86_64/raxml-ng --msa mafft\ alns/cytb_species.aln --model GTR+G
-./../methods/osx/raxml-ng_v1.1.0_macos_x86_64/raxml-ng --msa mafft\ alns/all_genes.aln --model GTR+G
+./../methods/osx/raxml-ng_v1.1.0_macos_x86_64/raxml-ng --msa mafft_alns/16s_species.aln --model GTR+G
+./../methods/osx/raxml-ng_v1.1.0_macos_x86_64/raxml-ng --msa mafft_alns/cytb_species.aln --model GTR+G
+
+echo "[Consensus Tree]"
+cat mafft_alns/16s_species.aln.raxml.bestTree > mafft_alns/all_species.gene.tre
+cat mafft_alns/cytb_species.aln.raxml.bestTree >> mafft_alns/all_species.gene.tre
+
+java -jar /Users/gillianchu/Desktop/cpsc/project/methods/osx/Astral/astral.5.7.8.jar -i /Users/gillianchu/Desktop/cpsc/project/datafiles/mafft_alns/all_species.gene.tre -o astral.consensus.tre
 
 echo "[Done]"
-
-
 
--- datafiles/select_genes.py
+++ datafiles/select_genes.py
@@ -1,15 +1,27 @@
 from Bio import SeqIO
 import os, glob
 
-folder_path = '/Users/gillianchu/Desktop/cpsc/project/datafiles/data'
+folder_path = '/Users/gillianchu/Desktop/cpsc/project/datafiles/data2'
+common_names = '/Users/gillianchu/Desktop/cpsc/project/datafiles/data2/speciesnames.txt'
+name_dict = dict()
+
+with open(common_names) as f:
+	lines = f.readlines()
+	for line in lines:
+		print(line)
+		before, after = line.split()
+		name_dict[tuple(before.split('_'))] = after
+
+print(name_dict)
+	
 print(folder_path)
 fasta_paths = glob.glob(os.path.join(folder_path, '*.fasta'))
 print(fasta_paths)
 
 seqs_16s = list()
 seqs_cytb = list()
-
 all_records = set()
+
 for fasta_path in fasta_paths:
 	gene = ''
 	#print(fasta_path)
@@ -30,7 +42,19 @@ for fasta_path in fasta_paths:
 
 		elif "cytb" in seq_record.id.lower():
 			gene = "cytb"
+	
+		sr_id = ''
+		if gene == "16s":
+			sr_id = tuple(seq_record.id.rstrip().split('_')[:2])
+		elif gene == "cytb":
+			sr_id = tuple(seq_record.id.rstrip().split('_')[:2])
+			#sr_id = seq_record.id.rstrip()[:-5]
 		
+		print(gene)
+		print(seq_record.id, sr_id)
+		print("becomes", name_dict[sr_id])
+		seq_record.id = name_dict[sr_id]
+
 		if gene == "16s":
 			seqs_16s.append(seq_record)	
 		elif gene == "cytb":
